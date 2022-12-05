69183ad7c86a2c7720c5ec5ed3bbf5f16d890939
training-dev <contact@training-dev.fr>
2022-12-05 11:55:45 +0100

    redirection

--- Components/Login.jsx
+++ Components/Login.jsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from 'react';
 import { Text, View, TextInput, Pressable, StyleSheet } from 'react-native';
-import { createUserWithEmailAndPassword,signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from '../Service/firebase';
 
 export default function Login({ setUser, navigation }) {
     const [login, setLogin] = useState(true);
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [accountCreated, setAccountCreated] = useState(false);
 
     function switchForm() {
         setLogin(!login);
@@ -16,21 +17,27 @@ export default function Login({ setUser, navigation }) {
         createUserWithEmailAndPassword(auth, email, password)
             .then((credential) => {
                 setLogin(true);
+                setAccountCreated(true);
             })
     }
 
-    function signin(){
+    function signin() {
         signInWithEmailAndPassword(auth, email, password)
-        .then((credential) => {
-            setUser(credential.user);
-            navigation.navigate("Home");
-        })
+            .then((credential) => {
+                setUser(credential.user);
+                navigation.navigate("Home");
+            })
     }
 
 
 
     return (
         <View style={styles.form}>
+            { accountCreated &&
+                <View>
+                    <Text>Compte créé, veuillez vous connecter</Text>
+                </View>
+            }
             <View style={styles.formGroup}>
                 <Text>Email</Text>
                 <TextInput style={styles.field} onChangeText={value => setEmail(value)}></TextInput>
